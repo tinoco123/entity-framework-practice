8dad89c0b50d82e9971ca65bbf051d325efad1b5
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-04-13 16:24:48 -0600

    Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace projectEF.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projectEF.Models
 {
     public class Category
     {
+        [Key]
         public Guid CategoryId { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage="El nombre de la tarea no debe exceder los 30 caracteres"), MinLength(3)]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public virtual ICollection<Task> Tasks { get; set; }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,14 +1,23 @@
-﻿namespace projectEF.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectEF.Models
 {
     public class Task
     {
+        [Key]
         public Guid TaskId { get; set; }
+        [ForeignKey("Category")]
         public Guid CategoriaId { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "El nombre de la tarea no debe exceder los 30 caracteres"), MinLength(3)]
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime DateCreated { get; set; }
         public Prioridad PrioridadTarea { get; set; }
         public virtual Category Categoria { get; set; }
+        [NotMapped]
+        public string Resume { get; set; }
     }
 
     public enum Prioridad
