2871b96dd4127bef3937e6edef41d19d6185e7ec
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-04-13 20:19:23 -0600

    Base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
