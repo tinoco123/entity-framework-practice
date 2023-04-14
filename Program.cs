22906d3df546eb2a0b1c470d94be6684e41d7041
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-04-13 23:28:26 -0600

    Conexion a base de datos desde appsettings.json

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+// builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+builder.Services.AddSqlServer<TasksContext>(builder.Configuration.GetConnectionString("TasksConnection"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -10,7 +11,7 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/dbconexion", async ([FromServices] TasksContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    return Results.Ok("Base de datos en SQLServer: " + dbContext.Database.IsSqlServer());
 });
 
 app.Run();
