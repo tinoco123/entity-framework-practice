145176db79b67f2ef8904594eedc9e2d1911bc1d
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-04-18 01:10:15 -0600

    Actualizar datos

--- Program.cs
+++ Program.cs
@@ -26,4 +26,32 @@ app.MapGet("/api/tareas", async([FromServices] TasksContext dbContext ) =>
     return Results.Ok(dbContext.Tasks.Include(p => p.Categoria).Where(p => p.Deadline > DateTime.Now));
 });
 
+app.MapPost("/api/tareas", async ([FromServices] TasksContext dbContext, [FromBody] projectEF.Models.Task task) =>
+{
+    task.TaskId = Guid.NewGuid();
+    task.DateCreated = DateTime.Now;
+    await dbContext.Tasks.AddAsync(task);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("api/tareas/{id}", async([FromServices] TasksContext dbContext, [FromBody] projectEF.Models.Task task, [FromRoute] Guid id) =>
+{
+    projectEF.Models.Task tareaActual = dbContext.Tasks.Find(id);
+    if (tareaActual != null)
+    {
+        tareaActual.Title = task.Title;
+        tareaActual.CategoriaId = task.CategoriaId;
+        tareaActual.Description = task.Description;
+        tareaActual.Deadline = task.Deadline;
+        tareaActual.PrioridadTarea = task.PrioridadTarea;
+
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+
+    }
+
+    return Results.NotFound();
+});
 app.Run();
